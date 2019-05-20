250c933fa2ea8ced10893dd12c58ffd68b7b4a9a
taoyeah <273861158@qq.com>
2019-05-20 23:22:45 +0800

    1

--- quant/check_volume.py
+++ quant/check_volume.py
@@ -1,11 +1,19 @@
 import tushare as ts
 import pandas as pd
+mystock = \
+    '603063.SH,' \
+    '000993.SZ,' \
+    '002797.SH,' \
+    '002008.SZ,' \
+    '601519.SH'
 dg = ts.get_realtime_quotes('601519') #Single stock symbol
 show1=dg[['code','name','price','bid','ask','volume','amount','time']]
 
-df = ts.get_sina_dd('601519', date='2019-05-17', vol=900)
-buy = df[df['type']=='买盘']
-sale = df[df['type']=='卖盘']
+df = ts.get_tick_data('002797', date='2019-05-20',src='tt')
+
+buy = df[(df['type']=='买盘')&(df['volume']>500)]
+sale = df[(df['type']=='卖盘')&(df['volume']>500)]
+
 buy_amount = buy.volume*buy.price
 buy_average_price = buy_amount.sum()/buy.volume.sum()
 
@@ -16,6 +24,7 @@ show = pd.DataFrame(columns=['buy','sale'],data=[[buy_average_price,sale_average
 
 print(show)
 print(show1)
+
 
 # print(buy_average_price)
 # print(buy.volume.sum())
