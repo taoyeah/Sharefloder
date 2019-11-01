b315fec193ea051c3a0d709e631f63e6c9d9a494
taoeyah <273861158@qq.com>
2019-11-01 16:16:30 +0800

    creat

--- quant/check_volume1.py
+++ quant/check_volume1.py
@@ -14,7 +14,7 @@ show1=dg[['code','name','price','bid','ask','volume','amount','time']]
 
 # df = ts.get_tick_data('002008', date='2019-09-19',src='tt')
 # df = ts.get_hist_data('601519', '2019-09-12','2019-09-17')
-df = ts.get_today_ticks('603063')
+df = ts.get_today_ticks('002008')
 
 buy1 = df[(df['volume'] > trig_volume) & (df['change'] > 0)]
 buy2 = df[(df['type'] == '买盘') & (df['volume'] > trig_volume) & (df['change'] == 0)]
@@ -43,3 +43,4 @@ print(buy.volume.sum())
 
 print(sale_average_price)
 print(sale.volume.sum())
+# df.to_csv('check_volume1.csv')
