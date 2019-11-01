01c89808afc37c64359c89823089068097e4c457
taoeyah <273861158@qq.com>
2019-11-01 16:16:13 +0800

    creat

--- quant/check_volume.py
+++ quant/check_volume.py
@@ -1,4 +1,5 @@
 import tushare as ts
+import matplotlib.pyplot as plt
 import pandas as pd
 mystock = \
     '603063.SH,' \
@@ -6,11 +7,12 @@ mystock = \
     '002797.SH,' \
     '002008.SZ,' \
     '601519.SH'
-dg = ts.get_realtime_quotes('601519') #Single stock symbol
+dg = ts.get_realtime_quotes('515000') #Single stock symbol
 show1=dg[['code','name','price','bid','ask','volume','amount','time']]
 
-# df = ts.get_tick_data('601519', date='2019-05-22',src='tt')
-df = ts.get_today_ticks('601519')
+df = ts.get_tick_data('601519', date='2019-09-12',src='tt')
+# df = ts.get_hist_data('601519', '2019-09-12','2019-09-16')
+# df = ts.get_today_ticks('603063')
 
 buy = df[(df['type']=='买盘')&(df['volume']>100)]
 sale = df[(df['type']=='卖盘')&(df['volume']>100)]
@@ -25,11 +27,11 @@ show = pd.DataFrame(columns=['buy','sale'],data=[[buy_average_price,sale_average
 print(" ")
 print(show)
 print(show1)
+# print(df['time'])
+
+print(buy_average_price)
+print(buy.volume.sum())
 
 
-# print(buy_average_price)
-# print(buy.volume.sum())
-#
-#
-# print(sale_average_price)
-# print(sale.volume.sum())
+print(sale_average_price)
+print(sale.volume.sum())
